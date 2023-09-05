f1333eb7a1b00cd6d9ab86f59e0727c493db79b2
Junior-SD <juniormwadime01@gmail.com>
2023-09-05 02:59:00 -0700

    Python - Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A Class Module
+
+Parameters:
+    void
+"""
+
+
+class LockedClass:
+    """Locked Class"""
+    __slots__ = ['first_name']
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def copy_list(my_list):
+    return[elements for elements in my_list
